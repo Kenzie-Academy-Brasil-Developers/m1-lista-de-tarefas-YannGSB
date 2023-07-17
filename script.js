e956479dcafe7f514fc1022a7d2be05ac3375d1a
unknown <yanngsb@hotmail.com>
2023-07-17 03:46:18 -0300

    tarefa finalizada

--- script.js
+++ script.js
@@ -21,7 +21,14 @@ function createCard(taskInfo) {
   const taskDescription = document.createElement("p");
 
   // Adicionando o titulo da tarefa como texto do paragrafo
-  p.innerText = taskInfo.titulo;
+
+  if (taskInfo.type === "Urgente") {
+    taskTitle.classList.add("span-urgent");
+  } else if (taskInfo.type === "Prioritário") {
+    taskTitle.classList.add("span-priority");
+  } else {
+    taskTitle.classList.add("normal");
+  }
 
   // Adicionando span e paragrafo a div
   taskCardContent.appendChild(taskTitle);
@@ -45,14 +52,59 @@ function renderElements(taskList) {
   htmlList.innerHTML = "";
 
   // Ajustar a lógica
-  let card = createCard(taskList[0]);
-  htmlList.appendChild(card);
-
-  card = createCard(taskList[1]);
-  htmlList.appendChild(card);
-
-  card = createCard(taskList[2]);
-  htmlList.appendChild(card);
+  for (let i = 0; i < taskList.length; i++) {
+    const task = taskList[i];
+    const card = createCard(task);
+    htmlList.appendChild(card);
+  }
 }
-
 renderElements(tasks);
+
+const buttonAddTask = document.querySelector("#btnSubmit");
+buttonAddTask.addEventListener("click", function (event) {
+  event.preventDefault();
+  const inputTitle = document.querySelector("#input_title");
+  const inputType = document.querySelector("#input_priority");
+  const title = inputTitle.value;
+  const type = inputType.value;
+
+  const newTask = {
+    title: title,
+    type: type,
+  };
+  tasks.push(newTask);
+  renderElements(tasks);
+});
+
+function createCard(taskInfo, i) {
+  const taskCardItem = document.createElement("li");
+  const taskCardContent = document.createElement("div");
+  const taskTitle = document.createElement("span");
+  const taskDescription = document.createElement("p");
+
+  if (taskInfo.type === "Urgente") {
+    taskTitle.classList.add("span-urgent");
+  } else if (taskInfo.type === "Prioritário") {
+    taskTitle.classList.add("span-priority");
+  } else {
+    taskTitle.classList.add("span-normal");
+  }
+
+  taskDescription.innerText = taskInfo.title;
+  taskCardContent.appendChild(taskTitle);
+  taskCardContent.appendChild(taskDescription);
+
+  const buttonDelete = document.createElement("button");
+
+  buttonDelete.innerHTML = '<i class="fa fa-trash" aria-hidden="true"></i>';
+
+  buttonDelete.addEventListener("click", function () {
+    tasks.splice(i, 1);
+    renderElements(tasks);
+  });
+
+  taskCardItem.appendChild(taskCardContent);
+  taskCardItem.appendChild(buttonDelete);
+
+  return taskCardItem;
+}
